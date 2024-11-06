1dc463dc3cd295df2e57987c11bf1539bb4a0a1e
Gennady Maltsev <gmaltsev@syrve.com>
2024-11-06 10:48:04 +0500

    Test push

---
+++ kill_dotnet.bat
@@ -0,0 +1,4 @@
+taskkill.exe /f /fi "imagename eq VBCSCompiler.exe"
+taskkill.exe /f /fi "imagename eq dotnet.exe"
+taskkill.exe /f /fi "imagename eq csc.exe"
+taskkill.exe /f /fi "imagename eq ServiceHub.RoslynCodeAnalysisService.exe"
