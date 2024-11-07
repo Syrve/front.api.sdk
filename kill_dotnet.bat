8239e0012084f1da993f05ba3aeb657ba4e488ac
Gennady Maltsev <gmaltsev@syrve.com>
2024-11-07 12:50:21 +0500

    Revert "Test push 2"
    This reverts commit e370befa509ac319b2fdde4155cf0bb1de90ebfd.

--- kill_dotnet.bat
+++
@@ -1,4 +0,0 @@
-taskkill.exe /f /fi "imagename eq VBCSCompiler.exe"
-taskkill.exe /f /fi "imagename eq dotnet.exe"
-taskkill.exe /f /fi "imagename eq csc.exe"
-taskkill.exe /f /fi "imagename eq ServiceHub.RoslynCodeAnalysisService.exe"
